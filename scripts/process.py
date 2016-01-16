eeefec96b5dee0ddd3a3ad8c619105450901d7c4
Konstantinos Iliakis <iliakiskostis@gmail.com>
2016-01-16 13:38:25 +0200

    process.py looks fine

---
+++ scripts/process.py
@@ -0,0 +1,30 @@
+import csv
+#import os
+import urllib
+#import dataconverters.xls
+sources = ["http://www3.epa.gov/climatechange/images/indicator_downloads/glaciers_fig-1.csv"]
+archive = 'archive/glaciers_fig-1.csv'
+data = 'data/glaciers.csv'
+
+def string_between(string, before, after):
+	temp = string.split(before)[1]
+	temp = temp.split(after)[0]
+	return temp
+
+def execute():
+    for s in sources:
+        urllib.urlretrieve(s, archive)
+
+    header = ['Year', 'Mean cumulative mass balance', 'Number of observations']
+    records = []
+
+    for line in open(archive):
+        records.append(line.split(','))
+    records =records[7:]
+    for r in records:
+        r[2] = r[2].strip()
+    writer = csv.writer(open(data, 'w'), lineterminator='\n')
+    writer.writerow(header)
+    writer.writerows(records)
+
+execute()
